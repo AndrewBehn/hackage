666586d63a40b33cff6a2682572f787a7098aefd
Julian Fleischer <julian.fleischer@fu-berlin.de>
2013-06-18 00:34:20 +0200

    Added bed-and-breakfast

---
+++ bed-and-breakfast/src/Numeric/Matrix/Dimension.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE Haskell2010
+    , TypeFamilies
+    , FlexibleContexts
+    , Trustworthy
+ #-}
+{-# OPTIONS -Wall #-}
+
+module Numeric.Matrix.Dimension
+
+import Numeric.Matrix
+
+newtype DimensionMatrix d s 
